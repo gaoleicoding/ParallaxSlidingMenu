648e657b2c3d5e1d144ad96a8b348974f7d14275
gaolei2 <GaoLei1201>
2016-01-25 11:16:27 +0800

    -m精简一些代码

--- ParallaxSlidingMenu(ViewGroup)/src/com/noter/layout/SlideLayout.java
+++ ParallaxSlidingMenu(ViewGroup)/src/com/noter/layout/SlideLayout.java
@@ -60,12 +60,12 @@ public class SlideLayout extends ViewGroup {
 		if (mScroller.computeScrollOffset()) {
 			scrollTo(mScroller.getCurrX(), mScroller.getCurrY());
 			scrollX=mScroller.getCurrX();
-//			Log.d("gaolei", "scrollX--------------------------------|"+scrollX);
+//			这里是判断打开菜单时即向右滑动菜单向右滑动 实现视觉差滑动效果，scrollX就是监听startScroll开始滚动程X的值，因为getScrollX()只是监听手在屏幕上滑动的过程，手离开后就要在这里监听，0.7是可以根据需求改动
 			if(scrollX<oldX){
 //			Log.d("gaolei", "scrollX2--------------------------------|"+scrollX);
 			ViewHelper.setTranslationX(getChildAt(0), (menuWidth+scrollX)* 0.7f);
 			}
-			Log.d("gaolei", "clickMenuToClose--------------------------------|"+clickMenuToClose);
+//			这里是判断点击菜单按钮关闭时，对菜单做处理，已达到视觉差效果，不然的话没有
 			if(clickMenuToClose){
 				ViewHelper.setTranslationX(getChildAt(0), (menuWidth+scrollX)* 0.7f);
 			}
@@ -106,13 +106,6 @@ public class SlideLayout extends ViewGroup {
 		
 	}
 	
-//	@Override
-//	protected void onLayout(boolean changed, int l, int t, int r, int b) {
-//		super.onLayout(changed, l, t, r, b);
-//		if(changed){
-//			mContent = getChildAt(0);
-//		}
-//	}
 	
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
@@ -142,7 +135,7 @@ public class SlideLayout extends ViewGroup {
 			if((curScrollX + deltaX) > 0){
 				deltaX = -curScrollX;
 			}
-			float scale = getScrollX() * 1.0f / menuWidth;
+//			这里是判断打手势滑动开菜单时即向右滑动菜单向右滑动 实现视觉差滑动效果，menuWidth+getScrollX()是菜单滑动时X的变化，手离开后就要在这里监听，0.7是可以根据需求改动
 		
 			ViewHelper.setTranslationX(getChildAt(0), (menuWidth+getScrollX())* 0.7f);
 			if (deltaX != 0) {
@@ -216,7 +209,6 @@ public class SlideLayout extends ViewGroup {
 	//关闭Menu布局
 	public void closeMenu() {
 		int curScrollX = getScrollX();
-		Log.d("gaolei", "getScrollX()---------closeMenu----------"+getScrollX());
 		scrollToDestination(-curScrollX);
 		isMenuOpen = false;
 	}
